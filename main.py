5560ad37fd4708e73f93df1c62358824c8b6572b
SharpestShark <46836426+SharpestShark@users.noreply.github.com>
2021-04-27 11:34:02 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import requests
-x = requests.get('https://w3schools.com/python/demopage.htm')
-
-print(x.text)
 import mymodule
 
 mymodule.greeting("Jonathan")
+
+print(mymodule.greeting("Jonathan")

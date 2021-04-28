0e0dcf537b62e992162229319c5c6001f6aa85f7
SharpestShark <46836426+SharpestShark@users.noreply.github.com>
2021-04-28 07:43:11 -0700

    Create funny.lua

---
+++ funny.lua
@@ -0,0 +1,29 @@
+local MarketplaceService = game:GetService("MarketplaceService")
+local Players = game:GetService("Players")
+ 
+local gamePassID = 0000000  -- Change this to your game pass ID
+ 
+local function onPlayerAdded(player)
+ 
+	local hasPass = false
+ 
+	-- Check if the player already owns the game pass
+	local success, message = pcall(function()
+		hasPass = MarketplaceService:UserOwnsGamePassAsync(player.UserId, gamePassID)
+	end)
+ 
+	-- If there's an error, issue a warning and exit the function
+	if not success then
+		warn("Error while checking if player has pass: " .. tostring(message))
+		return
+	end
+ 
+	if hasPass == true then
+		print(player.Name .. " owns the game pass with ID " .. gamePassID)
+		-- Assign this player the ability or bonus related to the game pass
+		--
+	end
+end
+ 
+-- Connect "PlayerAdded" events to the "onPlayerAdded()" function
+Players.PlayerAdded:Connect(onPlayerAdded)

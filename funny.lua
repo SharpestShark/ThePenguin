80b480c62e567f299c7afecc39c932c054c3cb3a
SharpestShark <46836426+SharpestShark@users.noreply.github.com>
2021-04-28 07:46:32 -0700

    Update funny.lua

--- funny.lua
+++ funny.lua
@@ -2,7 +2,19 @@ local MarketplaceService = game:GetService("MarketplaceService")
 local Players = game:GetService("Players")
  
 local gamePassID = 0000000  -- Change this to your game pass ID
+  
+-- Function to handle a completed prompt and purchase
+local function onPromptGamePassPurchaseFinished(player, purchasedPassID, purchaseSuccess)
  
+	if purchaseSuccess == true and purchasedPassID == gamePassID then
+		print(player.Name .. " purchased the game pass with ID " .. gamePassID)
+		-- Assign this player the ability or bonus related to the game pass
+		--
+	end
+end
+ 
+-- Connect "PromptGamePassPurchaseFinished" events to the "onPromptGamePassPurchaseFinished()" function
+MarketplaceService.PromptGamePassPurchaseFinished:Connect(onPromptGamePassPurchaseFinished)
 local function onPlayerAdded(player)
  
 	local hasPass = false
